891b5699b33d29e6cebb3816775c49bf467b3d2d
Ygor-Evaldt-dev <evaldtygor@gmail.com>
2023-09-07 17:48:35 -0300

    finalizado primeira implementação da rota 'books'

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ import app from "./src/app.js";
 import dotenv from "dotenv";
 dotenv.config();
 
-const { PORT } = process.env;
+const { PORT } = process.env || 3000;
 app.listen(PORT, () => {
     console.log(`API online no endereço http://localhost:${PORT}`);
-})
+});
 
